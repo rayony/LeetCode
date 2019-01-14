82acd2cee41ab128bf25abc604766e191d1f8ef4
Gary - VR2WCG <garychantzechun@hotmail.com>
2019-01-14 11:46:10 +0800

    Update s1.py

--- 529. Minesweeper/s1.py
+++ 529. Minesweeper/s1.py
@@ -5,10 +5,10 @@ class Solution:
 
 #define board = [][]
 
-#************************************************************	
+#************************************************************
     def countMine(self, board, click):
-    	#count and return the no. of adjacent mines in the selected cell
-    	"""
+        #count and return the no. of adjacent mines in the selected cell
+        """
         :type board: List[List[str]]
         :type click: List[int]
         :rtype: integer
@@ -19,26 +19,26 @@ class Solution:
         row = click[0]
         col = click[1]
 
-		count = 0
-		if (row-1) > 0:
-			if (col-1>0) and board[row-1][col-1] == 'M': count+=1
-			if board[row-1][col] == 'M': count+=1
-			if (col+1<max_col) and board[row-1][col+1] == 'M': count+=1
+        count = 0
+        if (row-1) > 0:
+            if (col-1>0) and board[row-1][col-1] == 'M': count+=1
+            if board[row-1][col] == 'M': count+=1
+            if (col+1<max_col) and board[row-1][col+1] == 'M': count+=1
 
-		if (col-1>0) and board[row][col-1] == 'M': count+=1
-		if (col+1<max_col) and board[row][col+1] == 'M': count+=1
+        if (col-1>0) and board[row][col-1] == 'M': count+=1
+        if (col+1<max_col) and board[row][col+1] == 'M': count+=1
 
-		if (row+1) <max_row:
-			if (col-1>0) and board[row+1][col-1] == 'M': count+=1
-			if board[row+1][col] == 'M': count+=1
-			if (col+1<max_col) and board[row+1][col+1] == 'M': count+=1
+        if (row+1) <max_row:
+            if (col-1>0) and board[row+1][col-1] == 'M': count+=1
+            if board[row+1][col] == 'M': count+=1
+            if (col+1<max_col) and board[row+1][col+1] == 'M': count+=1
 
-		return count
+        return count
 
 
-#************************************************************	
+#************************************************************    
     def updateBoard(self, board, click):
-    	#update board w.r.t selected cell, update nothing if invalid cell (out of boundary) selected
+        #update board w.r.t selected cell, update nothing if invalid cell (out of boundary) selected
         """
         :type board: List[List[str]]
         :type click: List[int]
@@ -52,38 +52,38 @@ class Solution:
 
 
         #case 1: update nothing if selected cell out of boundary
-		if 	(row-1) > 0 or\
-			(row+1<max_row) or \
-			(col-1) > 0 or \
-			(col+1<max_col): 
-			return board
+        if     (row-1) > 0 or\
+            (row+1<max_row) or \
+            (col-1) > 0 or \
+            (col+1<max_col): 
+            return board
 
-		#case 2: if selected cell is 'M', masking for unrevealed mines
-		if board[row,col] == 'M':	
-			#ToDo - update 'B' for mines in board other than board[row,col]
-			return board
+        #case 2: if selected cell is 'M', masking for unrevealed mines
+        if board[row,col] == 'M':    
+            #ToDo - update 'B' for mines in board other than board[row,col]
+            return board
 
-		#case 3: if selected cell is 'E', count the no. of mines in selected cell, reveal surrounding cells if count = 0
-		if board[row,col]  == 'E':
+        #case 3: if selected cell is 'E', count the no. of mines in selected cell, reveal surrounding cells if count = 0
+        if board[row,col]  == 'E':
 
-			board[row,col] = self.countMine(board, row,col)
+            board[row,col] = self.countMine(board, row,col)
 
-			if (board[row,col]==0):
-				board[row,col]='B'
-				self.updateBoard(board, (row-1,col-1))
-				self.updateBoard(board, (row-1,col))
-				self.updateBoard(board, (row-1,col+1))
+            if (board[row,col]==0):
+                board[row,col]='B'
+                self.updateBoard(board, (row-1,col-1))
+                self.updateBoard(board, (row-1,col))
+                self.updateBoard(board, (row-1,col+1))
 
-				self.updateBoard(board, (row,col-1))
-				self.updateBoard(board, (row,col+1))
+                self.updateBoard(board, (row,col-1))
+                self.updateBoard(board, (row,col+1))
 
-				self.updateBoard(board, (row+1,col-1))
-				self.updateBoard(board, (row+1,col))
-				self.updateBoard(board, (row+1,col+1))
+                self.updateBoard(board, (row+1,col-1))
+                self.updateBoard(board, (row+1,col))
+                self.updateBoard(board, (row+1,col+1))
 
-			return board
+            return board
 
-		#case: exception
-		print("Error")
-		return board
+        #case: exception
+        print("Error")
+        return board
 #************************************************************
