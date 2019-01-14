431dad66f8940161de3b195191b6bbeee5ab7a1e
Gary - VR2WCG <garychantzechun@hotmail.com>
2019-01-14 11:40:52 +0800

    Update s1.py

--- 529. Minesweeper/s1.py
+++ 529. Minesweeper/s1.py
@@ -14,20 +14,22 @@ class Solution:
 
         max_row = len(board)
         max_col = len(board[0])
+        row = click[0]
+        col = click[1]
 
 		count = 0
 		if (row-1) > 0:
-			if (col-1>0): if board[row-1][col-1] == 'M': count+=1
+			if (col-1>0) and board[row-1][col-1] == 'M': count+=1
 			if board[row-1][col] == 'M': count+=1
-			if (col+1<max_col): if board[row-1][col+1] == 'M': count+=1
+			if (col+1<max_col) and board[row-1][col+1] == 'M': count+=1
 
-		if (col-1>0): if board[row][col-1] == 'M': count+=1
-		if (col+1<max_col): if board[row][col+1] == 'M': count+=1
+		if (col-1>0) and board[row][col-1] == 'M': count+=1
+		if (col+1<max_col) and board[row][col+1] == 'M': count+=1
 
 		if (row+1) <max_row:
-			if (col-1>0): if board[row+1][col-1] == 'M': count+=1
+			if (col-1>0) and board[row+1][col-1] == 'M': count+=1
 			if board[row+1][col] == 'M': count+=1
-			if (col+1<max_col): if board[row+1][col+1] == 'M': count+=1
+			if (col+1<max_col) and board[row+1][col+1] == 'M': count+=1
 
 		return count
 
@@ -43,6 +45,9 @@ class Solution:
 
         max_row = len(board)
         max_col = len(board[0])
+        row = click[0]
+        col = click[1]
+
 
         #case 1: update nothing if selected cell out of boundary
 		if 	(row-1) > 0 or\
@@ -52,27 +57,27 @@ class Solution:
 			return board
 
 		#case 2: if selected cell is 'M', masking for unrevealed mines
-		if board(row,col) == 'M':	
+		if board[row,col] == 'M':	
 			#ToDo - update 'B' for mines in board other than board[row,col]
 			return board
 
 		#case 3: if selected cell is 'E', count the no. of mines in selected cell, reveal surrounding cells if count = 0
-		if board(row,col)  == 'E':
+		if board[row,col]  == 'E':
 
-			board(row,col) = self.countMine(board, row,col)
+			board[row,col] = self.countMine(board, row,col)
 
-			if (board(row,col)==0):
-				board(row,col)='B'
-				self.updateBoard(board, [row-1][col-1])
-				self.updateBoard(board, [row-1][col])
-				self.updateBoard(board, [row-1][col+1])
+			if (board[row,col]==0):
+				board[row,col]='B'
+				self.updateBoard(board, (row-1,col-1))
+				self.updateBoard(board, (row-1,col))
+				self.updateBoard(board, (row-1,col+1))
 
-				self.updateBoard(board, [row][col-1])
-				self.updateBoard(board, [row][col+1])
+				self.updateBoard(board, (row,col-1))
+				self.updateBoard(board, (row,col+1))
 
-				self.updateBoard(board, [row+1][col-1])
-				self.updateBoard(board, [row+1][col])
-				self.updateBoard(board, [row+1][col+1])
+				self.updateBoard(board, (row+1,col-1))
+				self.updateBoard(board, (row+1,col))
+				self.updateBoard(board, (row+1,col+1))
 
 			return board
 
